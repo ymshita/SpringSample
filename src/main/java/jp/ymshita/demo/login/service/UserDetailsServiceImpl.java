4a0cd12b904e7fbe82393aca02fee6210ed49c21
Yuya Yamashita <yama.98904@gmail.com>
2020-09-24 13:05:10 +0900

    update: ユーザー情報サービスの修正
    - パスワード更新メソッドの追加

--- src/main/java/jp/ymshita/demo/login/service/UserDetailsServiceImpl.java
+++ src/main/java/jp/ymshita/demo/login/service/UserDetailsServiceImpl.java
@@ -4,15 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import jp.ymshita.demo.login.domain.repository.LoginUserRepository;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Component("UserDetailsServiceImpl")
 public class UserDetailsServiceImpl implements UserDetailsService{
 	
 	@Autowired
 	private LoginUserRepository repository;
+
+	@Autowired
+	private PasswordEncoder encoder;
+
 
 	/**
 	 * @param: String userneme: user id
@@ -21,6 +30,21 @@ public class UserDetailsServiceImpl implements UserDetailsService{
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		UserDetails user = repository.selectOne(username);
 		return user;
+	}
+
+	/**
+	 * パスワード更新
+	 * @param userId
+	 * @param password
+	 * @throws ParseException
+	 */
+	public void updatePasswordDate(String userId, String password) throws ParseException {
+		String encryptPass = encoder.encode(password);
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+		Date date = sdf.parse("2099/12/31");
+
+		repository.updatePassword(userId, encryptPass, date);
 	}
 
 }
